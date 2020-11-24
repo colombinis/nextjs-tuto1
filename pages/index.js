64cf0c9cd4453e3dc910805a0a37639d7a70c566
Seba Colombini <sebastian.colombini@elpixel.com>
2020-11-24 11:14:39 -0300

    fix home description

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home({allPostData}) {
       <section className={utilStyles.headingMd}>
         <p>Magento, Wordpress fan...now moving to nextjs</p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
+         deploying to vercel platform - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
